91e6f7423e9e4e5e76946f45cd98cf9e596dd957
Austin Aluzzi <mrfrozenfirefly@gmail.com>
2023-05-25 12:50:26 -0700

    Integrate mongoDB for pixel data

--- server.js
+++ server.js
@@ -21,16 +21,51 @@ app.get("/*", (req, res) => {
     res.redirect("/");
 });
 
-let pixels = new Array(48).fill("ffffff").map(() => new Array(72).fill("ffffff"));
+const dotenv = require('dotenv').config();
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGODB_URI);
 
-io.on('connection', (socket) => {
-    console.log(socket.id + " connected");
-    io.to(socket.id).emit('load-data', pixels);
+async function init() {
+    const pixels = await loadPixels();
+    io.on('connection', (socket) => {
+        console.log(socket.id + " connected");
+        io.to(socket.id).emit('load-data', pixels);
 
-    socket.on('draw', (x, y, color) => {
-        if (x !== null && y !== null && x >= 0 && x < pixels[0].length && y >= 0 && y < pixels.length) {
-            socket.broadcast.emit('draw', x, y, color);
-            pixels[y][x] = color;
-        }
-    })
-});
+        socket.on('draw', (x, y, color) => {
+            if (x !== null && y !== null && x >= 0 && x < pixels[0].length && y >= 0 && y < pixels.length) {
+                socket.broadcast.emit('draw', x, y, color);
+                pixels[y][x] = color;
+            }
+        })
+
+        socket.on('disconnect', () => {
+            console.log(socket.id + " disconnected");
+            savePixels(pixels);
+        })
+    });
+}
+
+async function loadPixels() {
+    try {
+        console.log("Loading pixels from database");
+        const database = client.db('canvas');
+        const pixelColl = database.collection('pixels');
+        const result = await pixelColl.findOne({});
+        return result.pixels;
+    } catch (e) {
+        console.error(e, e.stack);
+    }
+}
+
+async function savePixels(pixels) {
+    try {
+        console.log("Saving pixels to database");
+        const database = client.db('canvas');
+        const pixelColl = database.collection('pixels');
+        await pixelColl.updateOne({}, { $set: { pixels: pixels } });
+    } catch (e) {
+        console.error(e, e.stack);
+    }
+}
+
+init();
